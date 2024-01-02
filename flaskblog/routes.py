0181b2c5dd40d91f90bad3a7d6b0d204cc2df02c
nzangi <nzangimuoki284@gmail.com>
2024-01-02 14:55:41 +0300

    done working on delete post function

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -131,3 +131,15 @@ def update_post(post_id):
         form.content.data = post.post_content
 
     return render_template('new_post.html', title="Update Post", form=form, legend="Update Post")
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post account has been deleted successfully!",'success')
+    return redirect(url_for('home'))
+
